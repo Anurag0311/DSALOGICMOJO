622385340add9ee627d0f76ec8d371de08aabd41
Anurag <anuragsingh.anurag1234@gmail.com>
2024-03-07 20:13:37 +0530

    added comments in BuySellProblem

--- Arrays/BuySellStock.java
+++ Arrays/BuySellStock.java
@@ -1,43 +1,61 @@
 package com.anurag.Arrays;
 
+import java.util.ArrayList;
 
-import java.util.ArrayList; 
-    
-public class BuySellStock { 
-  static class Result { 
-    int buy, sell; 
+//Class representing a solution for buying and selling stocks to maximize profit
+public class BuySellStock {
+
+ // Inner class to store the buy and sell indices as a result
+ static class Result {
+     int buy, sell;
+ }
+
+ // Method to find and print the buying and selling points to maximize profit
+ void findProfit(int array[], int n) {
+     // ArrayList to store the results (buying and selling points)
+     ArrayList<Result> result = new ArrayList<Result>();
+
+     // Loop through the array to find buying and selling points
+     for (int i = 0; i < array.length; i++) {
+         Result temp = new Result();
+
+         // Find the local minimum as the buying point
+         while ((i < n - 1) && (array[i + 1] <= array[i])) {
+             i++;
+         }
+
+         // Break if we reach the end of the array
+         if (i == n - 1) {
+             break;
+         }
+
+         temp.buy = i++;
+
+         // Find the local maximum as the selling point
+         while ((i < n - 1) && (array[i + 1] >= array[i])) {
+             i++;
+         }
+
+         // Set the selling point and add the result to the list
+         temp.sell = i;
+         result.add(temp);
+     }
+     
+     // Further processing can be done with the 'result' ArrayList if needed
+ }
+
+ // Main method to demonstrate the usage of the BuySellStock class
+ public static void main(String args[]) {
+     BuySellStock stock = new BuySellStock();
+
+     // Example input array representing stock prices on different days
+     int array[] = { 98, 178, 250, 300, 40, 540, 690 };
+     int n = array.length;
+
+     // Call the findProfit method to calculate and print buying and selling points
+     stock.findProfit(array, n);
+ }
 }
-    void findProfit(int array[], int n) 
-    { 
-        ArrayList<Result> result=new ArrayList<Result>();
-        for(int i=0;i<array.length;i++) {
-        	Result temp=new Result();
-        	while((i<n-1) && (array[i+1]<=array[i])) {
-        		i++;
-        	}
-        	if(i<n-1) {
-        		break;
-        	}
-        	temp.buy=i++;
-        	while((i<n) && (array[i+1] >= array[i])) {
-        		i++;
-        	}
-        	temp.sell=i;
-        	result.add(temp);
-        }
-         
-    } 
-  
-    public static void main(String args[]) 
-    { 
-        BuySellStock stock = new BuySellStock(); 
-  
-        int array[] = { 98, 178, 250, 300, 40, 540, 690 }; 
-        int n = array.length; 
-  
-        stock.findProfit(array, n); 
-    } 
-} 
 
 
 /* Try more Inputs
